a8ca87cf8f6964dabaec9f4d2f3ef9eecf11c1b9
Connor Cote <cotec4@gmail.com>
2020-08-13 00:18:00 -0600

    Working node but need to ensure I'm using the right file as well as creating the links and adding the badge

--- index.js
+++ index.js
@@ -16,13 +16,46 @@ async function writeReadMe (){
         },
         {
             type: "input",
-            message: "What is your background?",
-            name: "bio"
+            message: "Please provide installation instructions for your project.",
+            name: "installation"
         },
         {
             type: "input",
-            message: "What is your GitHub username",
-            name: "username"
+            message: "Please provide usage information.",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Please provide contribution guidelines.",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Please provide test instructions.",
+            name: "test"
+        },
+        {
+            type: "checkbox",
+            message: "Which license have you used in your application?",
+            name: "license",
+            choices: [
+                "MIT License",
+                "GNU AGPLv3",
+                "GNU GPLv3",
+                "GNU LGPLv3",
+                "Mozilla Public License 2.0",
+                "Apache License 2.0",
+            ]
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "gitHub"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
         }
     ])
     .then(function(response){
@@ -30,8 +63,47 @@ async function writeReadMe (){
 
         const projectTitle = response.projectTitle.charAt(0).toUpperCase() + response.projectTitle.slice(1);
         const description = response.description;
-        const userInput = `${projectTitle}:
-        `
+        const installation = response.installation;
+        const usage = response.usage;
+        const contribution = response.contribution;
+        const test = response.test;
+        const license = response.license;
+        const gitHub = response.gitHub;
+        const email = response.email;
+        const userInput = 
+        
+        `${projectTitle}
+
+        Description:
+        ${description}
+        
+        Table of Contents:
+        1. Installation
+        2. Usage
+        3. License
+        4. Contributing
+        5. Tests
+        6. Questions
+        
+        Installation:
+        ${installation}
+        
+        Usage:
+        ${usage}
+        
+        Licensing:
+        ${license}
+        
+        Contributing:
+        ${contribution}
+        
+        Tests:
+        ${test}
+        
+        Questions:
+        https://github.com/${gitHub}
+        
+        For any additional questions, please reach out to me at ${email}`
 
         fs.writeFile(fileName, userInput, function(err){
             if(err){
